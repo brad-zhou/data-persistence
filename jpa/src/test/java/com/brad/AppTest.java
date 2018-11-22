78963963bd8af11d1f1397122ce0039a20ada4e0
HKHF\zhoubd <9491319096@qq.com>
2018-11-22 15:40:06 +0800

    JPA持久化数据: 最少依赖（无Spring参与）

--- jpa/src/test/java/com/brad/AppTest.java
+++ jpa/src/test/java/com/brad/AppTest.java
@@ -1,20 +1,36 @@
 package com.brad;
 
-import static org.junit.Assert.assertTrue;
-
+import com.brad.domain.Department;
+import com.brad.domain.Employee;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testGetDepartmentById() {
+        new App().getDepartmentById(2L);
     }
+
+    @Test
+    public void testSaveDepartment() {
+        Department department = new Department();
+        department.setName("研发部");
+
+        Set<Employee> employees = new HashSet<>();
+        Employee employee1 = new Employee("brad", 10000, department);
+        Employee employee2 = new Employee("lily", 9000, department);
+        Employee employee3 = new Employee("lucy", 8000, department);
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+        department.setEmployees(employees);
+
+        new App().saveDepartment(department);
+    }
+
 }
